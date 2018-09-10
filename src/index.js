1f11cc3e1233decf7486ab9d60f4f9cc9da05a98
liuxj <liuxiajiang818@163.com>
2018-09-10 20:18:38 +0800

    0.2

--- src/index.js
+++ src/index.js
@@ -3,25 +3,33 @@ import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import ReactDOM from "react-dom";
 
+const appState = observable({
+	count:1,
+	handlecut(){
+		this.count--;
+	},
+	handleAdd(){
+		this.count++;
+	}
+})
 
 @observer
 class Test extends Component{
-	@observable count = 1;
 	handlecut(){
-		this.count--;
+		this.props.store.handlecut();
 	}
 	handleAdd(){
-		this.count++;
+		this.props.store.handleAdd();
 	}
 	render(){
 		return(
 			<div>
-					<div>count:{this.count}</div>
-					<button onClick ={this.handleAdd.bind(this)}>+</button>
-					<button onClick = {this.handlecut.bind(this)}>-</button>
+				<div>count:{this.props.store.count}</div>
+				<button onClick ={this.handleAdd.bind(this)}>+</button>
+				<button onClick = {this.handlecut.bind(this)}>-</button>
 			</div>
 		)
 	}
 }
 
-ReactDOM.render(<Test/>,document.getElementById("root"));
+ReactDOM.render(<Test store={appState}/>,document.getElementById("root"));
